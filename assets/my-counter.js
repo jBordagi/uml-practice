093ad65188a9087dfed2320479df13272b93483e
unknown <julien.bordagi@netcourrier.com>
2019-04-16 12:15:05 +0200

    devel

---
+++ assets/my-counter.js
@@ -0,0 +1,33 @@
+/**
+ * @name my-counter
+ * @author moi
+ * @version 1.0.0
+ * @description Gère un composant de type compteur
+ */
+
+ // Gestionnaire d'évenement sur le bouton + (id=increase-qty)
+
+ $('#increase-qty').on(
+     'click',
+     function(event) {
+    // J'essaie de lire la valeur du champs de saisie (input)
+    const inputField = $('#quantity');
+
+    // Récupérer la valeur max autoriséé: data max=n
+    const maxVal = inputField.data('max');
+    
+    // Introduire la contrainte fonctionnelle newVal<maxVal ... sinon NOOP
+
+    if (inputField.val()<maxVal) {
+
+       // Incrémenter la valeur de 1
+        let newVal= parseInt(inputField.val()) + 1;
+    // Afficher dans l'interface la nouvelle valeur
+        inputField.val(newVal)   
+    }
+    
+    // TODO: Afficher un message si maximum atteint 
+    //TODO: Désacativer le bouton + si maximum atteint
+
+     }
+ );
